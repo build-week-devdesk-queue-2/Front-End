dcbbdd24d06851e54d4a6cbe352b7bced05af281
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-19 01:13:56 -0500

    added login and register forms, no styling

--- front-end/README.md
+++ front-end/README.md
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
 Project Vision Doc:
 
 https://docs.google.com/document/d/1EFc_IXFpLRnDa3sK55S2ZTF5eFl9PWBeSkZA9sZxXX0/edit#heading=h.4oicemcbd57
-=======
-# bw-devdesk-queue-2
-
-Frontend for DevDesk Queue
-
-## Technical Design Documentation
-
-[TDD](https://docs.google.com/document/d/1EFc_IXFpLRnDa3sK55S2ZTF5eFl9PWBeSkZA9sZxXX0/)
->>>>>>> 8722105d31c84a24550e28b9830c6fec4606cc26
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Test</h1>
+      <Link to='/login'>Login</Link>
+      <Link to='/register'>Register</Link>
+      <Route path='/login' render={props => <Login {...props} />} />
+      <Route path='/register' render={props => <Register {...props} />} />
     </div>
   );
 }
