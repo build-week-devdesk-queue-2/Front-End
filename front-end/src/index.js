a85617da037404035d246e5669882509791ef473
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-18 21:22:39 -0500

    redux boilerplate

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducer from '../src/reducers/';
+
+import App from './App';
+import './index.css';
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
