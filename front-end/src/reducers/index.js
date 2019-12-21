5b21f5cc5a05b0dd1a95344572e9196ee5528574
Daniel Cruz <d.cruz.489@hotmail.com>
2019-12-20 21:38:05 -0700

    reducers, tickets, ticketlist, actions

--- front-end/src/reducers/index.js
+++ front-end/src/reducers/index.js
@@ -1,28 +1,8 @@
-import {
-  FETCH_TICKETS_START,
-  FETCH_TICKETS_SUCCESS,
-  FETCH_TICKETS_FAIL,
-  CREATE_TICKET_START,
-  CREATE_TICKET_SUCCESS,
-  CREATE_TICKET_FAIL,
-  DELETE_TICKET,
-  FILTER_TICKETS,
-} from '../Actions';
+import { combineReducers } from 'redux';
+import { tickets } from './tickets';
 
-import { initialState } from './initialState';
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case FETCH_TICKETS_START:
-      return {
-        ...state, 
-        loading: true
-      }
-
-
-    default:
-      return state;
-  }
-};
+const reducer = combineReducers({
+  tickets,
+})
 
 export default reducer;
