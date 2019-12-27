24dac9111bb90ad2c82bc62d8aec190939709b06
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-27 05:29:16 -0500

    Login form refactor

--- front-end/src/Components/Login.js
+++ front-end/src/Components/Login.js
@@ -5,51 +5,53 @@ import { useFormInput } from '../Helpers/useFormInput';
 import '../styles/login.css';
 
 const Login = props => {
-  const usernameInput = useFormInput('');
+	let initialState = {
+		username: '',
+		password: ''
+	};
 
-  const passwordInput = useFormInput('');
+	const { value: user, onChange } = useFormInput(initialState);
 
-  let user = {
-    username: usernameInput.value,
-    password: passwordInput.value
-  };
+	return (
+		<div className='login-contain'>
+			<div className='login-form'>
+				<h2>Login</h2>
 
-  return (
-    <div className='login-contain'>
-      <div className='login-form'>
-        <h2>Login</h2>
+				<form onSubmit={e => e.preventDefault()}>
+					<label>
+						U:
+						<input
+							type='text'
+							placeholder='Enter Username'
+							name='username'
+							value={user.username}
+							onChange={onChange}
+							required
+							minLength='4'
+						/>
+					</label>
 
-        <form>
-          <label>
-            U:
-            <input
-              type='text'
-              placeholder='Enter Username'
-              {...usernameInput}
-              required
-              minLength='4'
-            />
-          </label>
+					<label>
+						P:
+						<input
+							type='password'
+							placeholder='Enter Password'
+							name='password'
+							value={user.password}
+							onChange={onChange}
+							required
+							minLength='7'
+						/>
+					</label>
 
-          <label>
-            P:
-            <input
-              type='password'
-              placeholder='Enter Password'
-              {...passwordInput}
-              required
-              minLength='7'
-            />
-          </label>
-
-          <button className='login-btn' type='submit'>
-            Login
-          </button>
-        </form>
-        <Link to='/register'>Click here to register</Link>
-      </div>
-    </div>
-  );
+					<button className='login-btn' type='submit'>
+						Login
+					</button>
+				</form>
+				<Link to='/register'>Click here to register</Link>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
