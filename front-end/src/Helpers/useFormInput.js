cd7c3494c63a48b05526accc10e9930367cba20b
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-27 04:50:49 -0500

    better useFormInput hook

--- front-end/src/Helpers/useFormInput.js
+++ front-end/src/Helpers/useFormInput.js
@@ -1,10 +1,16 @@
 import { useState, useCallback } from 'react';
 
 export const useFormInput = initialValue => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = useCallback(
-    event => setValue(event.currentTarget.value),
-    []
-  );
-  return { value, onChange };
+	const [value, setValue] = useState(initialValue);
+
+	const onChange = useCallback(
+		event =>
+			setValue({
+				...value,
+				[event.currentTarget.name]: event.currentTarget.value
+			}),
+		[value]
+	);
+
+	return { value, onChange };
 };
