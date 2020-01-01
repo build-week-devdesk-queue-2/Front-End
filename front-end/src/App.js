24616c170b1310d2ad2edd6dfdb4d9b662f7d3f1
Jason Cruz <badmonkey.dev@gmail.com>
2020-01-01 12:32:22 -0600

    edited the README file to be useful for anyone needing to reference information, what to install etc. It is also helpful for current projects being worked on by othet students. Gives some insight. Also worked on ticket file, documented it as well.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,17 @@
 /** @jsx jsx */
-import { jsx, css } from '@emotion/core';
-import { Route, Link } from 'react-router-dom';
-import './App.css';
-import Login from './Components/Login';
-import Register from './Components/Register';
-import Navbar from './Components/Navbar';
+import { jsx, css } from "@emotion/core";
+import { Route, Link } from "react-router-dom";
+import "./App.css";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import Navbar from "./Components/Navbar";
+import Ticket from "./Components/Ticket";
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      <Ticket />
       <Route path='/login' render={props => <Login {...props} />} />
       <Route path='/register' render={props => <Register {...props} />} />
     </div>
