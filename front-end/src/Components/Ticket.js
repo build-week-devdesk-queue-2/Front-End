08cf26da0ffe29504f4455ede93ae13243ac90fb
indiMjc <45151346+indiMjc@users.noreply.github.com>
2020-01-01 23:28:41 -0500

    added urgency field to ticket form

--- front-end/src/Components/Ticket.js
+++ front-end/src/Components/Ticket.js
@@ -14,36 +14,43 @@ import 'semantic-ui-css/semantic.min.css';
 // prettier-ignore
 function Ticket() {
   return (
-    <Grid
-      textAlign='center'
-      style={{ height: "100vh", width: "80%", margin: "40px auto" }}
-      verticalAlign='middle'>
-      <Form>
-        <div>
-          <h1>Submit Ticket</h1>
-        </div>
-        <Form.Group widths='equal'>
-          <Form.Field control={Input} label='First Name' placeholder='First Name' />
-          <Form.Field control={Input} label='Last Name' placeholder='Last Name' />
+    <>
+      <h2 style={{ margin: "20px auto" }}>Username From State</h2>
+      <Grid
+        textAlign='center'
+        style={{ height: "100vh", width: "80%", margin: "0 auto 40px" }}
+        verticalAlign='middle'>
+        <Form>
+          <div>
+            <h1 style={{ marginBottom: "20px" }}>Submit Ticket</h1>
+          </div>
+          <Form.Group widths='equal'>
+            <Form.Field control={Input} label='Title' placeholder='Title' />
+            <Form.Field
+              control={Select}
+              label='Select Category'
+              options={categoryOptions}
+            />
+            <Form.Field
+              control={Select}
+              label="Urgency"
+              options={urgencyOptions}
+            />
+          </Form.Group>
           <Form.Field
-            control={Select}
-            label='Select Category'
-            options={categoryOptions}
+            control={TextArea}
+            label='Please tell us what issue you are currently experiencing, be elaborate.  Include steps you have already taken.'
+            placeholder='Description'
           />
-        </Form.Group>
-        <Form.Field
-          control={TextArea}
-          label='Please tell us what issue you are currently experiencing, be ellaborate.'
-          placeholder='Opinion'
-        />
-        <Form.Field
-          control={Button}
-          content='Submit'
-          label='By submitting you are agreeing that you do have an issue. Only submit one ticket at a time. We will contact you via email within 24 hrs.'
-          color='red'
-        />
-      </Form>
-    </Grid>
+          <Form.Field
+            control={Button}
+            content='Submit'
+            label='By submitting you are agreeing that you do have an issue. Only submit one ticket at a time. We will contact you via email within 24 hrs.'
+            color='red'
+          />
+        </Form>
+      </Grid>
+    </>
   );
 }
 
@@ -56,6 +63,12 @@ const categoryOptions = [
 	{ key: 'h', text: 'Hardware', value: 'hardware' },
 	{ key: 'sr', text: 'Site Related', value: 'siteRelated' },
 	{ key: 'm', text: 'Misc', value: 'misc' }
+];
+
+const urgencyOptions = [
+	{ key: 'hi', text: 'High', value: 'high' },
+	{ key: 'me', text: 'Medium', value: 'medium' },
+	{ key: 'lo', text: 'Low', value: 'low' }
 ];
 
 export default Ticket;
