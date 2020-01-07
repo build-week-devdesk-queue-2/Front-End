ae81ec3d0bf8275088d82432029f6719aac81664
Daniel Cruz <d.cruz.489@hotmail.com>
2020-01-06 21:34:26 -0700

    finished actions and reducers for create and delete tickets

--- front-end/src/reducers/tickets.js
+++ front-end/src/reducers/tickets.js
@@ -2,10 +2,10 @@ import {
 	FETCH_TICKETS_START,
 	FETCH_TICKETS_SUCCESS,
 	FETCH_TICKETS_FAIL,
-	// CREATE_TICKET_START,
-	// CREATE_TICKET_SUCCESS,
-	// CREATE_TICKET_FAIL,
-	// DELETE_TICKET,
+	CREATE_TICKET_START,
+	CREATE_TICKET_SUCCESS,
+	CREATE_TICKET_FAIL,
+	DELETE_TICKET,
 	// FILTER_TICKETS
 } from '../Actions';
 
@@ -16,14 +16,14 @@ const initialState = {
 export const tickets = (state = initialState, action) => {
 	switch (action.type) {
 		case FETCH_TICKETS_START:
-				console.log(state);
+			// console.log(state);
 			return {
 				...state,
 				loading: true,
 				error: ''
 			}
 		case FETCH_TICKETS_SUCCESS:
-			console.log(state);
+			// console.log(state);
 			return {
 				...state,
 				tickets: [...state.tickets, action.payload],
@@ -34,7 +34,35 @@ export const tickets = (state = initialState, action) => {
 			return {
 				...state,
 				loading: false,
+				error: action.payload
+			}
+		case CREATE_TICKET_START:
+			// console.log(state);
+			return {
+				...state,
+				loading: true,
+				error: ''
+			}
+		case CREATE_TICKET_SUCCESS:
+			console.log(state);
+			return {
+				...state,
+				tickets: [...state.tickets, action.payload],
+				loading: false,
+				error: ''
+			}
+		case CREATE_TICKET_FAIL:
+			return {
+				...state,
+				loading: false,
 				error: 'Error adding ticket :('
+			}
+		case DELETE_TICKET:
+			let newTickets = state.tickets.filter( ticket => ticket.id !== action.payload )
+			console.log(state.tickets)
+			return {
+				...state,
+				tickets: [...newTickets],
 			}
 
 		default:
